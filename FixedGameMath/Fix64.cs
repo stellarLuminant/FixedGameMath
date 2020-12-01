fbb1c6a4ef7ede7e2080c2a8474855b594f2c8ac
Stella <70691817+stellarLuminant@users.noreply.github.com>
2020-11-30 20:52:55 -0500

    Refactor unused reference

--- FixedGameMath/Fix64.cs
+++ FixedGameMath/Fix64.cs
@@ -3,7 +3,6 @@ using System.Diagnostics.CodeAnalysis;
 using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Text;
-using System.Text.RegularExpressions;
 
 namespace FixedGameMath
 {
